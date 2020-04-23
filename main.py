f2af7abe9bd0a4b3e21389259b5c03b115ad1c4a
Adam Frank <afrank@mozilla.com>
2020-04-23 13:18:35 -0700

    adding google profiler and some more work

--- main.py
+++ main.py
@@ -3,8 +3,20 @@
 from flask import Flask
 from multiprocessing import Pool
 from multiprocessing import cpu_count
+import googlecloudprofiler
+
+googlecloudprofiler.start(service='newrelic-python-demo',service_version='0.0.1',verbose=3)
 
 app = Flask(__name__)
+
+def f(l, i):
+    l.acquire()
+    print('worker ', i)
+    x = 0
+    while x < 1000:
+        print(x)
+        x += 1
+    l.release()
 
 @app.route("/")
 def hello():
@@ -15,3 +27,11 @@ def work():
     from math import factorial
     x = [ factorial(n) for n in range(1024) ]
     return str(x[-1])
+
+@app.route("/more/work/<count>")
+def morework(count):
+    from multiprocessing import Process, Lock
+    lock = Lock()
+    for num in range(int(count)):
+        Process(target=f, args=(lock, num)).start()
+    return "ok."
